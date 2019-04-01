57449814ea88f52fcce48a88a714e8ef0b0821a9
bsscco <bsscco@gmail.com>
2019-04-01 12:09:19 +0900

    Fix calculation bug for last month data.

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ app.get('/notify/yesterday/users', (req, res) => {
         })
         .then((rows) => {
             signInCnts.thisMonth = parseInt(rows[0].cnt);
-            return db.any(queries.getSignInCnt(moment(dates.yesterday).subtract(1, 'months').startOf('month'), moment(yesterday).subtract(1, 'months').set('date', today.get('date'))));
+            return db.any(queries.getSignInCnt(moment(dates.yesterday).subtract(1, 'months').startOf('month'), moment(yesterday).subtract(1, 'months').endOf('month').add(1, 'days')));
         })
         .then((rows) => signInCnts.lastMonth = parseInt(rows[0].cnt))
         .then(() => sendMsg('', makeNotiMsgPayload(dates, signInCnts)))
